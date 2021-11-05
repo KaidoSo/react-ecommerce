ffd6dc9ed03bc654ee13debc63802e508dc07d8b
Kaido Soomlais <kaido.soomlais@pipedrive.com>
2021-11-05 17:07:18 +0200

    install and import popper, bs and jquery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <div>Hei</div>;
+const element = <button class="btn btn-danger">Hei</button>;
 ReactDOM.render(element, document.getElementById('root'));
+
