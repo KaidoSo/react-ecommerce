fbf7d026b4113198351d00c100a24c230c8137e4
Kaido Soomlais <kaido.soomlais@pipedrive.com>
2021-11-25 16:59:09 +0200

    css and stuff

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// stateless functional component
-// always return JSX
+// CSS
+import "./index.css";
+const title = "21 Lessons for the 21st Century";
+const author = "Yuval Noah Harari";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/41SxL78DpvL._SX324_BO1,204,203,200_.jpg";
 
 function BookList() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="book-list">
+      <Book job="developer" />
     </section>
   );
 }
 
-const Book = () => {
-  return <article>this is a book</article>;
+const Book = (props) => {
+  console.log("🚀 ~ file: index.js ~ line 20 ~ Book ~ props", props);
+  return (
+    <article className="book">
+      <img src={img} alt="booke" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
 };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
